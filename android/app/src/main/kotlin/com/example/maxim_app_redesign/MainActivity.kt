6038482a57d0363db544547c9e8b25bf6f6864cf
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2023-03-15 09:23:36 +0700

    init project flutter

---
+++ android/app/src/main/kotlin/com/example/maxim_app_redesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maxim_app_redesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF32323D);
+const buttonActiveColor = Color(0xFF74728A);
+const whiteColor = Color(0xFFE5E5E5);
+const yellowColor = Color(0xFFF9C806);
+const greenColor = Color(0xFF6CC165);
+const redColor = Color(0xFFEF5052);
